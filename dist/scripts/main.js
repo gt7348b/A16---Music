73d8f103bc74be8092d73b598ce54aaa6efcf1b1
John Crocker <johnniecrocker@yahoo.com>
2014-11-04 18:26:44 -0500

    gulped

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,105 +1,152 @@
-var Song = Backbone.Model.extend({
-  defaults: {
-    title: '',
-    artist: '',
-    link: ''
-  },
+          window.App = {};
+          App.Models = {};
+          App.Collections = {};
+          App.Views = {};
 
-  idAttribute: '_id',
+(function(){
 
-  initialize: function(){
-  //  console.log('Lucky?')
-  }
+    App.Models.Song = Backbone.Model.extend({
+      defaults: {
+        title: '',
+        artist: '',
+        link: ''
+      },
 
-});
+      idAttribute: '_id',
 
-var Playlist = Backbone.Collection.extend({
-
-  model:  Song,
-  url:    'https://tiy-atl-fe-server.herokuapp.com/collections/jtc_playlist', 
-
-});
-
-var playlistView = Backbone.View.extend({
-
-  tagName: 'ul',
-  className: 'musiclist',
-
-  initialize: function(options){
-    //console.log('initialized');
-    this.render(options.list);
-  },
-
-  render: function(list){
-    var self = this;
-    var template = $('#songlist').html();
-    var render_song = _.template(template);
-
-    _.each(list.models, function(item){
-
-      self.$el.append(render_song(item.attributes));
+      initialize: function(){
+      //  console.log('Lucky?')
+      }
 
     });
 
-    $('#playlist').html(this.el);
+}());
 
-    return this;
-  },
+(function(){
 
-});
+      App.Collections.Playlist = Backbone.Collection.extend({
 
-//Create playlist
-
-var work_playlist = new Playlist();
-
-
-work_playlist.fetch().done(function(){
-
-
-  console.log(work_playlist);
-
-  var musicview = new playlistView({
-  //  console.log('hi');
-    list: work_playlist,
-
-    //console.log(list);
-  });
-  //console.log(musicview);
-//console.log(work_playlist);
-
-});
-
-$('.btn button').on('click', function(event){
-  event.preventDefault();
-
-  var newsong = new Song({
-    title: $('#input_title').val(),
-    artist: $('#input_artist').val(),
-    link:   $('#input_link').val()
-
-  });
-
-    //  console.log(newsong);
-
-  work_playlist.add(newsong);
-
-  //console.log(work_playlist);
-
-  newsong.save();
-
-//  $("#input_title")[0].reset();
-//  $("#input_artist")[0].reset();
-//  $("#input_link")[0].reset();
-
-  work_playlist.fetch().done(function(){
-
-    var musicview = new playlistView({
-      list: work_playlist,
+      model:  App.Models.Song,
+      url:    'https://tiy-atl-fe-server.herokuapp.com/collections/jtc_playlist',
 
     });
 
-  });
+}());
+
+(function(){
+
+    App.Views.playlistView = Backbone.View.extend({
+
+      tagName: 'ul',
+      className: 'musiclist',
+
+      events: {
+        'click li': 'deleteSong'
+      },
+
+      initialize: function(){
+        //console.log('initialized');
+        this.render(App.work_playlist);
+
+        App.work_playlist.on('sync', this.render, this);
+        App.work_playlist.on('destry', this.render, this);
+      },
+
+      render: function(){
+        var self = this;
+        var template = $('#songlist').html();
+        var render_song = _.template(template);
+
+        _.each(App.work_playlist.models, function(item){
+
+          self.$el.append(render_song(item.attributes));
+
+          //console.log(item.id);
+
+        });
+
+        $('#playlist').html(this.el);
+
+        return this;
+      },
+
+      deleteSong: function(event){
+        event.preventDefault();
+
+        var id = $(event.target).attr('id');
+
+        //console.log(id);
+
+        var eliminate = App.work_playlist.get(id);
+
+        console.log(eliminate);
+
+        eliminate.destroy();
+
+      }
+
+    });
+
+}());
+
+(function(){
+
+    App.Views.AddSong = Backbone.View.extend({
+
+        el: '#addnew',
+
+        events: {
+          'submit #songInput': 'addNewSong'
+        },
+
+        initialize: function(){
+          this.render();
+        },
+
+        render: function(){
+          this.$el.html($('#newsong').html());
+        },
+
+        addNewSong: function(e){
+          e.preventDefault();
+
+          var newsong = new App.Models.Song({
+            title: $('#input_title').val(),
+            artist: $('#input_artist').val(),
+            link:   $('#input_link').val()
+
+          });
+
+          console.log(newsong);
+
+          App.work_playlist.add(newsong);
+
+          console.log(App.work_playlist);
+
+          newsong.save();
+
+          $('#songInput')[0].reset();
+        },
+
+    })
+
+}());
+
+(function(){
+
+    //Create playlist
+
+    App.work_playlist = new App.Collections.Playlist();
 
 
+    App.work_playlist.fetch().done(function(){
+      //console.log(App.work_playlist);
+      App.musicview = new App.Views.playlistView({
+      });
 
-});
+    });
+
+    new App.Views.AddSong();
+
+
+}());
