39b6f253a9bed4ae0999da1de2defe6d72db88e1
John Crocker <johnniecrocker@yahoo.com>
2014-11-04 18:08:08 -0500

    form posting

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,39 +14,5 @@
 
     new App.Views.AddSong();
 
-    $('.btn button').on('click', function(event){
-      event.preventDefault();
-
-      var newsong = new Song({
-        title: $('#input_title').val(),
-        artist: $('#input_artist').val(),
-        link:   $('#input_link').val()
-
-      });
-
-        //  console.log(newsong);
-
-      work_playlist.add(newsong);
-
-      //console.log(work_playlist);
-
-      newsong.save();
-
-    //  $("#input_title")[0].reset();
-    //  $("#input_artist")[0].reset();
-    //  $("#input_link")[0].reset();
-
-      work_playlist.fetch().done(function(){
-
-        var musicview = new playlistView({
-          list: work_playlist,
-
-        });
-
-      });
-
-
-
-    });
 
 }());
--- app/scripts/views/add_views.js
+++ app/scripts/views/add_views.js
@@ -4,6 +4,9 @@
 
         el: '#addnew',
 
+        events: {
+          'submit #songInput': 'addNewSong'
+        },
 
         initialize: function(){
           this.render();
@@ -11,6 +14,27 @@
 
         render: function(){
           this.$el.html($('#newsong').html());
+        },
+
+        addNewSong: function(e){
+          e.preventDefault();
+
+          var newsong = new App.Models.Song({
+            title: $('#input_title').val(),
+            artist: $('#input_artist').val(),
+            link:   $('#input_link').val()
+
+          });
+
+          console.log(newsong);
+
+          App.work_playlist.add(newsong);
+
+          console.log(App.work_playlist);
+
+          newsong.save();
+
+
         },
 
     })
